aa1367bc0392600adde8785b6e19b685e275b87f
Angel Medrano <asmedrano@gmail.com>
2012-09-24 01:17:55 -0400

    Completes Page style

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package WordPress
- * @subpackage HTML5_Boilerplate
+ * @subpackage NE-Workcamp
  */
 
 get_header(); ?>
@@ -10,18 +10,14 @@ get_header(); ?>
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <article class="post" id="post-<?php the_ID(); ?>">
     <header>
-      <h2><?php the_title(); ?></h2>
+      <h2 class="title font-blue"> <?php the_title(); ?></h2>
     </header>
-  
-    <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-
-    <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-  
+     <div class="content">
+       <?php the_content('... Read the rest'); ?>
+    </div>
+ 
   </article>
   <?php endwhile; endif; ?>
-  <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
-
-  <?php comments_template(); ?>
 
 </div>
 
